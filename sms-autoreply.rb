4119bbade863ae76ee98dcba812d54a87ddfda71
Qifan Jin <qifanj@andrew.cmu.edu>
2017-04-14 11:24:30 -0400

    edit message body

--- sms-autoreply.rb
+++ sms-autoreply.rb
@@ -4,7 +4,9 @@ require 'sinatra'
 
 get '/sms-autoreply' do
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Message "Here is the survey link: https://goo.gl/xTnoY5"
+  	r.Message do |message|
+        message.Body "Here is the survey link: https://goo.gl/xTnoY5, thank you for your participation!"
+        end
   end
   twiml.text
 end
