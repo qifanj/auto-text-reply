39d1c24e371c8d2cc80bad280ab24d290235c01c
Qifan Jin <qifanj@andrew.cmu.edu>
2017-04-14 11:07:28 -0400

    add link

--- sms-autoreply.rb
+++ sms-autoreply.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 
 get '/sms-autoreply' do
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Message "Hey Monkey. Thanks for the message!"
+    r.Message "Here is the survey link: https://goo.gl/xTnoY5"
   end
   twiml.text
 end
