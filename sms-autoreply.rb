df1f9672cc4e3900d4b89998702536974d98ef8e
Qifan Jin <qifanj@andrew.cmu.edu>
2017-04-19 13:10:00 -0400

    edit text reply

--- sms-autoreply.rb
+++ sms-autoreply.rb
@@ -5,7 +5,7 @@ require 'sinatra'
 get '/sms-autoreply' do
   twiml = Twilio::TwiML::Response.new do |r|
   	r.Message do |message|
-        message.Body "Here is the survey link: https://goo.gl/xTnoY5, thank you for your participation!"
+        message.Body "Please take this one-minute survey to help us improve the airport and you will have a chance to win a $250 visa gift card! Survey link: https://goo.gl/xTnoY5 . Thank you for your participation!"
         end
   end
   twiml.text
